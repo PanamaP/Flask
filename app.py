bada33c51da60d987ddfce3b9001d330f91be2bc
PanamaP <elfarsnaer@gmail.com>
2020-01-16 02:33:02 +0000

    First commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return render_template("layout.html", framework="Flask")
+
+@app.route("/spurningar", methods=["GET"])
+def httpSpurningar():
+    return render_template("httpTemplate.html")
+
+@app.route("/httpBasic")
+def httpBasic():
+    return render_template('httpBasic.html')
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
+ 
